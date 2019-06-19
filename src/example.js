bea835c809ed5a8fc4b719d85baa2e85a11b6ec1
Evandro Machado <evandro.machado@southsystem.com.br>
2019-06-19 15:38:13 -0300

    Ajustes de minificação do código final

--- src/example.js
+++ src/example.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 
 export default class ExampleComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1,
+      interval: null,
+    };
+  }
+
+  componentDidMount() {
+    const interval = setInterval(() => {
+      this.setState((state, props) => ({ counter: state.counter + 1 }));
+    }, 1000);
+    this.setState({ interval });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
   render() {
-    return <h1>Example</h1>
+    const { counter } = this.state;
+    return <h1>Example {counter}</h1>;
   }
 }
